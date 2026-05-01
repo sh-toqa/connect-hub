7ce97f65ed50838f4559fd6a9b427615656ebe5f
sh-toqa <toqashalash8@gmail.com>
2026-05-02 00:08:56 +0300

    setup app testing configuration

---
+++ frontend/src/setupTests.js
@@ -0,0 +1,4 @@
+// src/setupTests.js
+// Extends Vitest's expect with @testing-library/jest-dom matchers
+// e.g. toBeInTheDocument(), toBeDisabled(), toHaveValue() …
+import '@testing-library/jest-dom';
