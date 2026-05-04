a215aff84f77a92822ef81919c68a162957d6697
sh-toqa <toqashalash8@gmail.com>
2026-05-04 23:09:44 +0300

    remove unused import

--- backend/src/main/java/org/connecthub/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/connecthub/backend/security/SecurityConfig.java
@@ -18,9 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import org.connecthub.backend.repository.UserRepository;
-
 import java.util.List;
 
 /**
