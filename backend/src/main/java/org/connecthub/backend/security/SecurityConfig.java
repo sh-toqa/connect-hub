0f90e73486aee5e6ec0fcc0b7257d2356a22c638
sh-toqa <toqashalash8@gmail.com>
2026-09-18 03:34:59 +0300

    remove unused import

--- backend/src/main/java/org/connecthub/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/connecthub/backend/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package org.connecthub.backend.security;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.connecthub.backend.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
